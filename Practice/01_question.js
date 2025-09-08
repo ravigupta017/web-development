0a91ce599edc9bdc0f45c05e3f39ff881973417a
web_dev <raviguptaa017@gmail.com>
2025-09-08 12:17:36 +0530

    add question files

---
+++ Practice/01_question.js
@@ -0,0 +1,8 @@
+let number_1=Number(prompt("Enter First Number"));
+let number_2=Number(prompt("Enter Second Number"));
+console.log("1st + 2nd = ", number_1 + number_2);
+console.log("1st - 2nd = ", number_1 - number_2);
+console.log("1st * 2nd = ", number_1 * number_2);
+console.log("1st / 2nd = ", number_1 / number_2);
+console.log("1st % 2nd = ", number_1 % number_2);
+
