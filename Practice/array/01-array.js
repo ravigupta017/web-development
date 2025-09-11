bf870b097589c6124c3f30b6503e66bdb79ba3a5
web_dev <raviguptaa017@gmail.com>
2025-09-11 17:00:01 +0530

    array files

---
+++ Practice/array/01-array.js
@@ -0,0 +1,50 @@
+// // // LITERAL METHOD DEFINING ARRAY
+// // // array number 
+// // let arrNum=[10,20, 11, 55];
+// // // console.log(arrNum)
+
+// // // array string
+// // let arrstr=["Ravi", "amit", "rahul"];
+// // // console.log(arrstr)
+
+// // // array string wtih number1
+// // let arrStrNum=["Ravi", 55, "amit", "rahul"];
+// // // console.log(arrStrNum)
+
+// // // array into array
+// // let arrInArr=[[10, 20, "rohit"]];
+// // // console.log(arrInArr)
+
+// // // CONSTRUCTER METHOD ARRAY DEFINING
+// // let arrConst = new Array(11);
+// // // console.log(arrConst);
+
+// // // array typeoff is an object
+// // let arrTypeof=[[10, 20, "rohit"]];
+// // console.log(typeof arrInArr);
+
+// // // note: arrays are mutable you change it 
+// // // for loop in array 
+// // // let arrLoop=[10, 20, 30, "ravi", "120", ["arr"]  ];
+// // for (i = 0; i < arrLoop.length; i++) {
+// // //   console.log(arrLoop[i]*2);
+// // }
+
+
+// // // for loop in array 
+// // // let arrLoop=[10, 20, 30, "ravi", "120", ["arr"]  ];
+// // // for (i = 0; i < arrLoop.length; i++) {
+// // //   console.log(arrLoop[i]*2);
+// // }
+
+// // // forof loop in array 
+// // // let arrForof=[10, 20, 30, "ravi", "120", ["arr"]  ];
+// // // for(let elem of arrForof){
+// //     // console.log(elem);
+// // }
+
+// // forof loop in array 
+// let arrForin=[10, 20, 30, "ravi", "120", ["arr"]  ];
+// for(let index in arrForin){
+//   console.log(index);
+// }
---
+++ Practice/array/array_methods.js
@@ -0,0 +1,14 @@
+// push method used to add new element  into array in last postion and its return a new lenght of array
+let arr=[10, 20 , 30 , 40 , "ravi"]
+arr.push(55,"rohit", 11);
+// checking what its return
+let retrn = arr.push(55, "rohit", 11); /* return a new lenght of array*/
+console.log(retrn) 
+// console.log(arr);
+
+let arr1 = [10, 20,30, 40, ];
+arr1.pop(); /* pop.() method is remove element from array in the last postion*/
+console.log(arr1)
+let retrn01=arr1.pop();
+// console.log(retrn01)
+
