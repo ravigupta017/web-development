d4d240c4019031deefce1fa1e7ae8aef23f14b03
web_dev <raviguptaa017@gmail.com>
2025-09-24 09:09:14 +0530

    loops

--- Practice/loops/loops.js
+++ Practice/loops/loops.js
@@ -1,8 +1,7 @@
 // simple for loop
 for (let i= 0; i < 5; i++) {
     console.log(i);
-    
-}
+  }
 
 
 // while loop
@@ -18,3 +17,19 @@ do {
     console.log(i);
     i++;
 }while (i<=5)
+
+// forof loop (for acess characters)
+let str="Ravi Gupta";
+for (let ch of str) {
+    console.log(ch);
+     }
+
+// forin loop (for acess key_value )
+let str1="Ravi Gupta";
+for(let key in str){
+    console.log(key)
+}
+
+// foreach loop with fat arrow funtions(working with function)
+const array = ["ravi", "rahul", "amit"];
+array.forEach((element) => console.log(element));
